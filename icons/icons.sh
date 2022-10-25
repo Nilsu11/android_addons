49cf11877aec82633a087a4b0e5faecd02072bf9
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-10-25 19:47:34 +0200

    finish cam
    DO NOT USE YET

--- icons/icons.sh
+++ icons/icons.sh
@@ -1,3 +1,6 @@
-sed -i "s/'    <background android:drawable="@color/ic_launcher_background" />'/'    <background android:drawable="@color/ic_background" />'/g" ic_launchercamera
-sed -i "s/'@drawable/ic_launcher_monochrome'/'@drawable/ic_launcher_foreground'/g" ic_launchercamera
-cp -f camera.xml acl
+sed -i "s/'    <background android:drawable="@color/ic_launcher_background" />'/'    <background android:drawable="@color/ic_background" />'/g" ~/modLOS/Aperture/app/src/main/res/mipmap-anydpi/ic_launcher.xml
+sed -i "s/'@drawable/ic_launcher_monochrome'/'@drawable/ic_launcher_foreground'/g" ~/Aperture/app/src/main/res/mipmap-anydpi/ic_launcher.xml
+cp -f camera.xml ~/modLOS/Aperture/app/src/main/res/drawable/ic_launcher_foreground.xml
+sed -i '$d' ~/modLOS/Aperture/app/src/main/res/values/colors.xml
+echo '    <color name="ic_background">#68be6c</color>' >> ~/modLOS/Aperture/app/src/main/res/values/colors.xml
+echo '</resources>' >> ~/modLOS/Aperture/app/src/main/res/values/colors.xml
