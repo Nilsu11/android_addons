9b66f436d33156c9dd4ec1e5bffb35390f29d488
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-10-26 19:07:43 +0200

    DO NOT USE YET

--- icons/icons.sh
+++ icons/icons.sh
@@ -1,5 +1,5 @@
 sed -i "s/'    <background android:drawable="@drawable/ic_launcher_background" />'/'    <background android:drawable="@color/ic_background" />'/g" ~/modLOS/Aperture/app/src/main/res/mipmap-anydpi/ic_launcher.xml
-sed -i "s/'@drawable/ic_launcher_monochrome'/'@drawable/ic_launcher_foreground'/g" ~/Aperture/app/src/main/res/mipmap-anydpi/ic_launcher.xml
+sed -i "s/'@drawable/ic_launcher_monochrome'/'@drawable/ic_launcher_foreground'/g" ~/modLOS/Aperture/app/src/main/res/mipmap-anydpi/ic_launcher.xml
 cp -f camera.xml ~/modLOS/Aperture/app/src/main/res/drawable/ic_launcher_foreground.xml
 sed -i '$d' ~/modLOS/Aperture/app/src/main/res/values/colors.xml
 echo '    <color name="ic_background">#68be6c</color>' >> ~/modLOS/Aperture/app/src/main/res/values/colors.xml
@@ -12,3 +12,4 @@ cp -f recorderb.xml ~/modLOS/Recorder/app/src/main/res/drawable/ic_launcher_back
 cp -f etar.xml ~/modLOS/Etar/app/src/main/res/drawable/ic_launcher_background.xml
 cp -f calcb.xml ~/modLOS/ExactCalculator/res/drawable/ic_launcher_background.xml
 cp -f calcf.xml ~/modLOS/ExactCalculator/res/drawable/ic_launcher_foreground.xml
+cp -f clock.xml ~/modLOS/DeskClock/res/drawable/ic_launcher_foreground.xml
