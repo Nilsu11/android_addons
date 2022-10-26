bfb229c7bfbafeea957ced2cebdc47b73972f3f8
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-10-26 15:33:34 +0200

    DO NOT USE YET

--- icons/icons.sh
+++ icons/icons.sh
@@ -1,4 +1,4 @@
-sed -i "s/'    <background android:drawable="@color/ic_launcher_background" />'/'    <background android:drawable="@color/ic_background" />'/g" ~/modLOS/Aperture/app/src/main/res/mipmap-anydpi/ic_launcher.xml
+sed -i "s/'    <background android:drawable="@drawable/ic_launcher_background" />'/'    <background android:drawable="@color/ic_background" />'/g" ~/modLOS/Aperture/app/src/main/res/mipmap-anydpi/ic_launcher.xml
 sed -i "s/'@drawable/ic_launcher_monochrome'/'@drawable/ic_launcher_foreground'/g" ~/Aperture/app/src/main/res/mipmap-anydpi/ic_launcher.xml
 cp -f camera.xml ~/modLOS/Aperture/app/src/main/res/drawable/ic_launcher_foreground.xml
 sed -i '$d' ~/modLOS/Aperture/app/src/main/res/values/colors.xml
@@ -7,3 +7,9 @@ echo '</resources>' >> ~/modLOS/Aperture/app/src/main/res/values/colors.xml
 cp -f musicb.xml ~/modLOS/Eleven/res/drawable/ic_launcher_background.xml
 cp -f musicf.xml ~/modLOS/Eleven/res/drawable/ic_launcher_foreground.xml
 cp -f audiofx.xml ~/modLOS/Audiofx/res/drawable/ic_launcher_background.xml
+cp -f recorderf.xml ~/modLOS/Recorder/app/src/main/res/drawable/ic_launcher_foreground.xml
+cp -f recorderb.xml ~/modLOS/Recorder/app/src/main/res/drawable/ic_launcher_background.xml
+cp -f etarf.xml ~/modLOS/Etar/app/src/main/res/drawable/ic_launcher_foreground.xml
+cp -f etarb.xml ~/modLOS/Etar/app/src/main/res/drawable/ic_launcher_background.xml
+cp -f calcb.xml ~/modLOS/ExactCalculator/res/drawable/ic_launcher_background.xml
+cp -f calcf.xml ~/modLOS/ExactCalculator/res/drawable/ic_launcher_foreground.xml
