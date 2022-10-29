0878189f1eb61a177439758fc2bf7ee6ddf77208
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-10-29 19:28:33 +0200

    Create dev.sh

---
+++ dev.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/LineageOS/android_packages_apps_Aperture.git ~/modLOS/Aperture
+git clone https://github.com/LineageOS/android_packages_apps_Eleven.git ~/modLOS/Eleven
+git clone https://github.com/LineageOS/android_packages_apps_Etar.git ~/modLOS/Etar
+git clone https://github.com/LineageOS/android_packages_apps_ExactCalculator.git ~/modLOS/ExactCalculator
+git clone https://github.com/LineageOS/android_packages_apps_Recorder.git ~/modLOS/Recorder
+sh ExactCalculator.sh
+sh ic_dev.sh
