02fa1320dc3736c1f3d1c5586daa04b460a47d17
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-10-30 23:25:07 +0100

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,7 +1,6 @@
 git clone https://github.com/LineageOS/android_packages_apps_Aperture.git ~/modLOS/Aperture
 git clone https://github.com/LineageOS/android_packages_apps_Eleven.git ~/modLOS/Eleven
 git clone https://github.com/LineageOS/android_packages_apps_Etar.git ~/modLOS/Etar
-git clone https://github.com/LineageOS/android_packages_apps_ExactCalculator.git ~/modLOS/ExactCalculator
 git clone https://github.com/LineageOS/android_packages_apps_Recorder.git ~/modLOS/Recorder
 git clone https://github.com/LineageOS/android_packages_apps_Jelly.git ~/modLOS/Jelly
 sh ExactCalculator.sh
