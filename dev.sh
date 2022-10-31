488ae75c6e474d2dc51d04d4ac2b54a95ce9cbb4
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-10-31 12:20:44 +0100

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -4,4 +4,5 @@ git clone https://github.com/LineageOS/android_packages_apps_Etar.git ~/modLOS/E
 git clone https://github.com/LineageOS/android_packages_apps_Recorder.git ~/modLOS/Recorder
 git clone https://github.com/LineageOS/android_packages_apps_Jelly.git ~/modLOS/Jelly
 sh ExactCalculator.sh
+sh OmniClock.sh
 sh icons/ic_dev.sh
