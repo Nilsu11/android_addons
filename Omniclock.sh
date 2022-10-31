485bd0913972e22a937b3059eba329059ef3aa06
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-10-31 17:38:18 +0100

    Update Omniclock.sh

--- Omniclock.sh
+++ Omniclock.sh
@@ -3,7 +3,7 @@ git clone https://github.com/Nilsu11/android_packages_apps_OmniClock.git -b grad
 sed -i 's/org.omnirom.deskclock.R.animator.fade/org.omnirom.deskclock.R.anim.abc_fade/' ~/modLOS/OmniClock/src/org/omnirom/deskclock/widget/ActionableToastBar.java
 sed -i 's/public class AlarmClockFragment extends DeskClockFragment implements/public abstract class AlarmClockFragment extends DeskClockFragment implements/' ~/modLOS/OmniClock/src/org/omnirom/deskclock/AlarmClockFragment.java
 sed -i '/@Override/d' ~/modLOS/OmniClock/src/org/omnirom/deskclock/AlarmClockFragment.java
-sed -i '/import org.omnirom.deskclock.R;/d' ~/modLOS/OmniClock/src/org/omnirom/deskclock/worldclock/AddCityCialog.java
+sed -i '/import org.omnirom.deskclock.R;/d' ~/modLOS/OmniClock/src/org/omnirom/deskclock/worldclock/AddCityDialog.java
 sed -i '/import org.omnirom.deskclock.R;/d' ~/modLOS/OmniClock/src/org/omnirom/deskclock/AlarmPluginFactory.java
 sed -i '/import org.omnirom.deskclock.R;/d' ~/modLOS/OmniClock/src/org/omnirom/deskclock/Utils.java
 sed -i '/package/a import org.omnirom.deskclock.R;' ~/modLOS/OmniClock/src/org/omnirom/deskclock/**.java
