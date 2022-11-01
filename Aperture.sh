c280c39b69242607a8e67d33396bbc6ddd024e55
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-11-01 11:16:11 +0100

    icons to file

---
+++ Aperture.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/LineageOS/android_packages_apps_Aperture.git ~/modLOS/Aperture
+sed -i "s/'    <background android:drawable="@drawable/ic_launcher_background" />'/'    <background android:drawable="@color/ic_background" />'/g" ~/modLOS/Aperture/app/src/main/res/mipmap-anydpi/ic_launcher.xml
+sed -i "s/'@drawable/ic_launcher_monochrome'/'@drawable/ic_launcher_foreground'/g" ~/modLOS/Aperture/app/src/main/res/mipmap-anydpi/ic_launcher.xml
+cp -f ~/modLOS/android_addons/icons/camera.xml ~/modLOS/Aperture/app/src/main/res/drawable/ic_launcher_foreground.xml
+sed -i '$d' ~/modLOS/Aperture/app/src/main/res/values/colors.xml
+echo '    <color name="ic_background">#68be6c</color>' >> ~/modLOS/Aperture/app/src/main/res/values/colors.xml
+echo '</resources>' >> ~/modLOS/Aperture/app/src/main/res/values/colors.xml
