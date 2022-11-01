c2ad1aec587e9f7d9c0d27171b2bcd61bb31bf21
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-11-01 11:10:23 +0100

    Update ExactCalculator.sh

--- ExactCalculator.sh
+++ ExactCalculator.sh
@@ -2,3 +2,6 @@ git clone https://github.com/LineageOS/android_packages_apps_ExactCalculator.git
 git clone https://github.com/Nilsu11/ExactCalcGradle.git ~/modLOS/ExactCalculator/gradle
 sed -i '/package/a import org.lineageos.calculator2.R;' android_packages_apps_ExactCalculator/src/com/android/calculator2/**.java
 git clone https://android.googlesource.com/platform/external/crcalc ~/modLOS/ExactCalculator/crcalc
+cp -f calcb.xml ~/modLOS/ExactCalculator/res/drawable/ic_launcher_background.xml
+cp -f calcf.xml ~/modLOS/ExactCalculator/res/drawable/ic_launcher_foreground.xml
+sed -i 's/com.android.calculator2/org.lineageos.calculator2/' ~/modLOS/ExactCalculator/gradle/build.gradle
