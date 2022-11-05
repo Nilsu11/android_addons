0a1f178e9ef3ea6bf351ef52d2317d3984b66445
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-11-05 12:03:00 +0100

    DO NOT USE YET (IT IS NOT TESTED)

--- jelly.sh
+++ jelly.sh
@@ -10,3 +10,12 @@ sed '/^$/d' ~/modLOS/Jelly/adblock/adaway.txt
 paste -d '\n' ~/modLOS/Jelly/adblock/adaway.txt ~/modLOS/Jelly/adblock/quarks.txt ~/modLOS/Jelly/adblock/jquarks.txt > ~/modLOS/Jelly/app/src/main/res/raw/hosts.txt
 awk '!seen[$0]++' ~/modLOS/Jelly/app/src/main/res/raw/hosts.txt
 wget -O ~/modLOS/Jelly/app/src/main/java/org/lineageos/jelly/utils/AdBlocker.java https://raw.githubusercontent.com/CarbonROM/android_packages_apps_Quarks/cr-7.0/app/src/main/java/org/carbonrom/quarks/utils/AdBlocker.java
+sed -i "s/org.carbonrom.quarks/org.lineageos.jelly/g" ~/modLOS/Aperture/app/src/main/res/mipmap-anydpi/ic_launcher.xml
+sed -i '/package/a import org.lineageos.jelly.utils.AdBlocker;' ~/modLOS/Jelly/app/src/main/java/org/lineageos/jelly/MainActivity.java
+PrefsUtils.getHomePage
+sed -i '/PrefsUtils.getHomePage/a AdBlocker.init(this);' ~/modLOS/Jelly/app/src/main/java/org/lineageos/jelly/MainActivity.java
+sed -i '/public final class PrefsUtils/a private static final String KEY_ADBLOCKER = "key_adblocker";' ~/modLOS/Jelly/app/src/main/java/org/lineageos/jelly/utils/PrefsUtils.java
+echo 'public static boolean getAdBlocker(Context context) {' >> ~/modLOS/Jelly/app/src/main/java/org/lineageos/jelly/utils/PrefsUtils.java
+echo 'SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);' >> ~/modLOS/Jelly/app/src/main/java/org/lineageos/jelly/utils/PrefsUtils.java
+echo 'return prefs.getBoolean(KEY_ADBLOCKER, true);' >> ~/modLOS/Jelly/app/src/main/java/org/lineageos/jelly/utils/PrefsUtils.java
+echo '}' >> ~/modLOS/Jelly/app/src/main/java/org/lineageos/jelly/utils/PrefsUtils.java
