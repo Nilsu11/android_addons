2c04355567025570b640e2e9048f0ffc0b9f39ef
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-11-19 15:31:49 +0100

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,13 +1,12 @@
-git clone https://github.com/LineageOS/android_packages_apps_Eleven.git ~/modLOS/Eleven
 git clone https://github.com/LineageOS/android_packages_apps_Etar.git ~/modLOS/Etar
 git clone https://github.com/LineageOS/android_packages_apps_Recorder.git ~/modLOS/Recorder
-git clone https://github.com/LineageOS/android_packages_apps_Jelly.git ~/modLOS/Jelly
 sh ~/modLOS/android_addons/Aperture.sh
-sh ~/modLOS/android_addons/Browser.sh
 sh ~/modLOS/android_addons/Eleven.sh
-sh ~/modLOS/android_addons/Etar.sh
+sh ~/modLOS/android_addons/Etar.sh #does not exist
 sh ~/modLOS/android_addons/ExactCalculator.sh
+# sh ~/modLOS/android_addons/Games.sh #this file and the app does not exist and is not prioritized, everything else is
+sh ~/modLOS/android_addons/Jelly.sh
 sh ~/modLOS/android_addons/OmniClock.sh
-sh ~/modLOS/android_addons/Recorder.sh
-sh ~/modLOS/android_addons/local.properties.sh
-sh icons/ic_dev.sh
+sh ~/modLOS/android_addons/Recorder.sh #does not exist
+sh ~/modLOS/android_addons/local.properties.sh #does not exist
+sh icons/ic_dev.sh #will be removed
