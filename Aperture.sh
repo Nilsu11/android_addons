3d372a9552031209cda11e7793d13ecfa9157182
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-11-19 15:02:54 +0100

    Update Aperture.sh

--- Aperture.sh
+++ Aperture.sh
@@ -1,6 +1,5 @@
 git clone https://github.com/LineageOS/android_packages_apps_Aperture.git ~/modLOS/Aperture
 sed -i "s/'    <background android:drawable="@drawable/ic_launcher_background" />'/'    <background android:drawable="@color/ic_background" />'/g" ~/modLOS/Aperture/app/src/main/res/mipmap-anydpi/ic_launcher.xml
-sed -i "s/'@drawable/ic_launcher_monochrome'/'@drawable/ic_launcher_foreground'/g" ~/modLOS/Aperture/app/src/main/res/mipmap-anydpi/ic_launcher.xml
 cp -f ~/modLOS/android_addons/icons/camera.xml ~/modLOS/Aperture/app/src/main/res/drawable/ic_launcher_foreground.xml
 cp -f ~/modLOS/android_addons/icons/cameram.xml ~/modLOS/Aperture/app/src/main/res/drawable/ic_launcher_monochrome.xml
 sed -i '$d' ~/modLOS/Aperture/app/src/main/res/values/colors.xml
