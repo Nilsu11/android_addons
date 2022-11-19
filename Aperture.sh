8fb487f80c256d41867f703ca0869abdbec8a82a
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-11-19 14:38:27 +0100

    other icon theme

--- Aperture.sh
+++ Aperture.sh
@@ -3,5 +3,5 @@ sed -i "s/'    <background android:drawable="@drawable/ic_launcher_background" /
 sed -i "s/'@drawable/ic_launcher_monochrome'/'@drawable/ic_launcher_foreground'/g" ~/modLOS/Aperture/app/src/main/res/mipmap-anydpi/ic_launcher.xml
 cp -f ~/modLOS/android_addons/icons/camera.xml ~/modLOS/Aperture/app/src/main/res/drawable/ic_launcher_foreground.xml
 sed -i '$d' ~/modLOS/Aperture/app/src/main/res/values/colors.xml
-echo '    <color name="ic_background">#68be6c</color>' >> ~/modLOS/Aperture/app/src/main/res/values/colors.xml
+echo '    <color name="ic_background">#2A3232</color>' >> ~/modLOS/Aperture/app/src/main/res/values/colors.xml
 echo '</resources>' >> ~/modLOS/Aperture/app/src/main/res/values/colors.xml
