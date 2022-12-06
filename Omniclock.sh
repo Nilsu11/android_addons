bb49c5b40fcd886f4e8b9e2b0c0acc579d1df3d9
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-12-06 16:24:33 +0100

    Update Omniclock.sh

--- Omniclock.sh
+++ Omniclock.sh
@@ -1,12 +1,10 @@
-git clone https://github.com/Nilsu11/android_packages_apps_OmniClock.git -b 10 ~/modLOS/OmniClock
-git clone https://github.com/Nilsu11/android_packages_apps_OmniClock.git -b gradle ~/modLOS/OmniClock/gradle
-sed -i 's/org.omnirom.deskclock.R.animator.fade/org.omnirom.deskclock.R.anim.abc_fade/' ~/modLOS/OmniClock/src/org/omnirom/deskclock/widget/ActionableToastBar.java
-sed -i 's/public class AlarmClockFragment extends DeskClockFragment implements/public abstract class AlarmClockFragment extends DeskClockFragment implements/' ~/modLOS/OmniClock/src/org/omnirom/deskclock/AlarmClockFragment.java
-sed -i '/@Override/d' ~/modLOS/OmniClock/src/org/omnirom/deskclock/AlarmClockFragment.java
-sed -i '/import org.omnirom.deskclock.R;/d' ~/modLOS/OmniClock/src/org/omnirom/deskclock/worldclock/AddCityDialog.java
-sed -i '/import org.omnirom.deskclock.R;/d' ~/modLOS/OmniClock/src/org/omnirom/deskclock/AlarmPluginFactory.java
-sed -i '/import org.omnirom.deskclock.R;/d' ~/modLOS/OmniClock/src/org/omnirom/deskclock/Utils.java
-sed -i '/package/a import org.omnirom.deskclock.R;' ~/modLOS/OmniClock/src/org/omnirom/deskclock/**.java
-sed -i '/package/a import org.omnirom.deskclock.R;' ~/modLOS/OmniClock/src/org/omnirom/deskclock/**/**.java
-sed -i '/import org.omnirom.deskclock.R;/d' ~/modLOS/OmniClock/src/org/omnirom/deskclock/widget/SlidingTabStrip.java
-sed -i 's/final TimePickerDialog timePickerFragment/TimePickerDialog timePickerFragment/' ~/modLOS/OmniClock/src/org/omnirom/deskclock/AlarmUtils.java
+git clone https://github.com/Nilsu11/android_packages_apps_OmniClock.git -b 10 ~/modLOS/OmniClock/Clock
+git clone https://github.com/Nilsu11/android_packages_apps_OmniClock.git -b gradle ~/modLOS/OmniClock
+sed -i 's/org.omnirom.deskclock.R.animator.fade/org.omnirom.deskclock.R.anim.abc_fade/' ~/modLOS/OmniClock/Clock/src/org/omnirom/deskclock/widget/ActionableToastBar.java
+sed -i '/import org.omnirom.deskclock.R;/d' ~/modLOS/OmniClock/Clock/src/org/omnirom/deskclock/worldclock/AddCityDialog.java
+sed -i '/import org.omnirom.deskclock.R;/d' ~/modLOS/OmniClock/Clock/src/org/omnirom/deskclock/AlarmPluginFactory.java
+sed -i '/import org.omnirom.deskclock.R;/d' ~/modLOS/OmniClock/Clock/src/org/omnirom/deskclock/Utils.java
+sed -i '/package/a import org.omnirom.deskclock.R;' ~/modLOS/OmniClock/Clock/src/org/omnirom/deskclock/**.java
+sed -i '/package/a import org.omnirom.deskclock.R;' ~/modLOS/OmniClock/Clock/src/org/omnirom/deskclock/**/**.java
+sed -i '/import org.omnirom.deskclock.R;/d' ~/modLOS/OmniClock/Clock/src/org/omnirom/deskclock/widget/SlidingTabStrip.java
+sed -i 's/final TimePickerDialog timePickerFragment/TimePickerDialog timePickerFragment/' ~/modLOS/OmniClock/Clock/src/org/omnirom/deskclock/AlarmUtils.java
