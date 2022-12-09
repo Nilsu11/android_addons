bed0139df7da608a95d0bc8a7f0b5176f1ca0b43
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-12-09 21:19:32 +0100

    Create Eleven.sh

--- Eleven.sh
+++ Eleven.sh
@@ -5,3 +5,4 @@ sed -i '$d' ~/modLOS/Eleven/res/values/colors.xml
 echo '    <color name="ic_background">#2A3232</color>' >> ~/modLOS/Eleven/res/values/colors.xml
 echo '</resources>' >> ~/modLOS/Eleven/res/values/colors.xml
 cp -f ~/modLOS/android_addons/icons/musicf.xml ~/modLOS/Eleven/res/drawable/ic_launcher_foreground.xml
+sed -i "s/'ic_launcher_monochrome'/'ic_launcher_foreground'/g" ~/modLOS/Eleven/res/mipmap-anydpi/ic_launcher.xml
