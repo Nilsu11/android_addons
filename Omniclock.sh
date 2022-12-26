19db8fa1c2b9bd2e5b045addf0f419fb2e1da547
Nilsu11 <106374285+Nilsu11@users.noreply.github.com>
2022-12-26 17:06:40 +0100

    fix git error

--- Omniclock.sh
+++ Omniclock.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/Nilsu11/android_packages_apps_OmniClock.git -b 10 ~/modLOS/OmniClock/Clock
 git clone https://github.com/Nilsu11/android_packages_apps_OmniClock.git -b gradle ~/modLOS/OmniClock
+git clone https://github.com/Nilsu11/android_packages_apps_OmniClock.git -b 10 ~/modLOS/OmniClock/Clock
 sed -i 's/org.omnirom.deskclock.R.animator.fade/org.omnirom.deskclock.R.anim.abc_fade/' ~/modLOS/OmniClock/Clock/src/org/omnirom/deskclock/widget/ActionableToastBar.java
 sed -i '/import org.omnirom.deskclock.R;/d' ~/modLOS/OmniClock/Clock/src/org/omnirom/deskclock/worldclock/AddCityDialog.java
 sed -i '/import org.omnirom.deskclock.R;/d' ~/modLOS/OmniClock/Clock/src/org/omnirom/deskclock/AlarmPluginFactory.java
